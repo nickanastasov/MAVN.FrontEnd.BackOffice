cf9538e9cca206eca6e93cb3ecb85b6829e819f3
Vitalii Dasaev <vitalii.dasaev@lykke-business.ch>
2020-04-24 08:34:25 +0300

    #51 remove public layout from login page

--- src/app/authentication/authentication-routing.module.ts
+++ src/app/authentication/authentication-routing.module.ts
@@ -1,28 +1,21 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {PublicLayoutComponent} from '../public/public-layout/public-layout/public-layout.component';
 import {LoginPageComponent} from './login-page/login-page.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: PublicLayoutComponent,
-    children: [
-      {
-        path: '',
-        pathMatch: 'full',
-        redirectTo: 'login'
-      },
-      {
-        path: 'login',
-        component: LoginPageComponent
-      }
-    ]
-  }
+    pathMatch: 'full',
+    redirectTo: 'login',
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AuthenticationRoutingModule {}
