c401596e41226e89187b2c018df60e6ef8558551
Aleksandr <Aleksandr.Taranishin@wrenkitchens.com>
2020-05-07 19:04:56 +0300

    Bind backend

--- src/app/admin/partners/services/payment-providers.service.ts
+++ src/app/admin/partners/services/payment-providers.service.ts
@@ -1,6 +1,8 @@
 import {Injectable} from '@angular/core';
 import {ApiHttpService} from 'ngx-api-utils';
 import {PaymentProvidersResponse} from '../models/payment-providers-response.interface';
+import {HttpParams} from '@angular/common/http';
+import {Provider, PaymentDetails} from '../models/provider.interface';
 @Injectable({
   providedIn: 'root',
 })
@@ -12,7 +14,19 @@ export class PaymentProvidersService {
     return this.apiHttp.get<PaymentProvidersResponse>(this.apiPath + '/properties');
   }
 
-  create(model: any) {
-    return this.apiHttp.post<any>(this.apiPath, model);
+  create(model: Provider) {
+    return this.apiHttp.post<Provider>(this.apiPath, model);
+  }
+  checkPaymentIntegration(id: string) {
+    const params = new HttpParams().set('PartnerId', encodeURIComponent(id));
+    return this.apiHttp.get<any>(`${this.apiPath}/integration/check`, {params: params});
+  }
+  getById(id: string) {
+    const params = new HttpParams().set('partnerId', encodeURIComponent(id));
+    return this.apiHttp.get<any>(this.apiPath, {params: params});
+  }
+
+  update(model: Provider) {
+    return this.apiHttp.put<Provider>(this.apiPath, model);
   }
 }
